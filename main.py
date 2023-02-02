3497bd28b7cbc79c5f8dea4eae4742930086d52d
bella <yiseuljeong0415@gmail.com>
2023-02-02 12:18:11 +0900

    update main.py to test

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
-import logging
-from sqlalchemy.orm import Session
-from database.db_config import SessionLocal, engine
-from typing import List
-from fastapi import Depends, FastAPI, HTTPException
+# import logging
+# from sqlalchemy.orm import Session
+# from database.db_config import SessionLocal, engine
+# from typing import List
+# from fastapi import Depends, FastAPI, HTTPException
 
-logging.basicConfig()  # 쿼리 로그를 남기기
+# logging.basicConfig()  # 쿼리 로그를 남기기
 
-logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO) 
+# logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO) 
+
+# app = FastAPI()
+
+# def get_db() :
+#     db = SessionLocal()
+    
+#     try : 
+#         yield db # 비동기 프로그래밍 
+        
+#     finally :
+#         db.close() # db닫음
+        
+# @app.get("/")
+# def hello() :
+#     return {"message" : "hello"}
+
+from typing import Union
+
+from fastapi import FastAPI
 
 app = FastAPI()
 
-def get_db() :
-    db = SessionLocal()
-    
-    try : 
-        yield db # 비동기 프로그래밍 
-        
-    finally :
-        db.close() # db닫음
-        
+
 @app.get("/")
-def hello() :
-    return {"message" : "hello"}
+def read_root():
+    return {"Hello": "World"}
