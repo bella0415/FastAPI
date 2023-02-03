eacc957ad920c70580d10aef06fccb940e9a08fa
bella <yiseuljeong0415@gmail.com>
2023-02-03 12:35:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -29,15 +29,6 @@ from fastapi import FastAPI
 # CORSMiddleware 을 사용하여 FastAPI 응용 프로그램의 교차 출처 리소스 공유 환경을 설정할 수 있음
 app = FastAPI()
 
-origins = ['http://172.31.39.223/']
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "Bella"}
